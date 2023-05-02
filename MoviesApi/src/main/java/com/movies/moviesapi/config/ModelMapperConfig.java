7e6ce48f809d5293ca32ae0b99bc2e1cee7ed639
niz-ka <kamil.niznik2000@gmail.com>
2023-05-02 18:27:46 +0200

    feat: add model mapper config

---
+++ MoviesApi/src/main/java/com/movies/moviesapi/config/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package com.movies.moviesapi.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
